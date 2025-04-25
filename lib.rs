08793d55785a78a00ba6f6b2fc459c4b9ff2c1c0
Houman Shadab <hshadab@users.noreply.github.com>
2025-04-25 11:12:59 -0400

    zkEngine_dev/lib.rs

---
+++ lib.rs
@@ -0,0 +1,19 @@
+//! Nebula based zkVM for WASM ISA
+#![allow(clippy::type_complexity)]
+#![allow(non_snake_case)]
+#![deny(missing_docs)]
+pub mod aggregation;
+pub mod error;
+pub mod sharding;
+pub mod utils;
+pub mod wasm_ctx;
+pub mod wasm_snark;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use wasmi_wasi::WasiCtx;
+
+// re-export
+pub use nova;
